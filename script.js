850726956d843481d09d51a5cd0850e2a386738d
iamkokilahiran <priyadarshanarakh@gmail.com>
2025-05-10 12:28:09 +0530

    Refactor code to clarify variable naming and structure for message sending and file upload functionality

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const chatBody = document.querySelector('.chat-body');
 const messageInput = document.querySelector('.message-input');
-const sendButton = document.querySelector('#send-message');
-const file
+const sendMessageButton = document.querySelector('#send-message');
+const fileInput = document.querySelector('#file-input');
+const fileUploadWrapper = document.querySelector('.file-upload-wrapper');
+const fileCancelButton = document.querySelector('#file-cancel');
+
+const API_KEY = `AIzaSyA9BUSQL_cJfLsKLwxnfkrVUsiDA_PZ1MI`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
